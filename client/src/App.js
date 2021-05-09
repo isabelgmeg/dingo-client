fa6f06f04a499b1557697478b033879f5a625c59
Isabelgarcia <isabelgmegino@gmail.com>
2021-05-09 22:14:49 +0200

    added some mixin styles and finish splash page

--- client/src/App.js
+++ client/src/App.js
@@ -3,32 +3,28 @@ import { Route, Link } from "react-router-dom";
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
 
-import Picture from "./components/Picture.jsx";
 import logo from "./assets/dingo_logo.png";
 
-import "./App.css";
-
+import "./App.scss";
 
 function App() {
   return (
     <div className="App">
       <div className="splash_container">
-        <div className="splash_container_logo">
-          
-          <Link to="/">
-          <img src={logo} alt={logo} />
-          </Link>
-        </div>
+        <div className="splash_content">
+        <Link className="splash_container_logo" to="/">
+          <img classNmae="splash_container_logo_image" src={logo} alt={logo} />
+        </Link>
         <h1>Getting Healthy made easy</h1>
-        <Route path="/register">
+        <Link className="splash_container_button" to="/register">
           <button className="splash_container_registerButton">
-          <RegisterPage />
+            Register!🔥
           </button>
-        </Route>
-
-        <Route path="/login">
-          <LoginPage />
-        </Route>
+        </Link>
+        <Link to="/login">
+        <a className="splash_container_link" href="/login">Sign in</a>
+        </Link>
+      </div>
       </div>
     </div>
   );
