ccc4428c89f9c1f9ebc04468f07d3ab44e97ae27
Isabelgarcia <isabelgmegino@gmail.com>
2021-05-11 12:39:43 +0200

    solved controller with help from stackoverflow

--- client/src/components/BiometricForm/BiometricForm.jsx
+++ client/src/components/BiometricForm/BiometricForm.jsx
@@ -1,36 +1,36 @@
-import { useState } from "react";
-import { useForm, Controller } from "react-hook-form";
+import React from "react";
 
-import SliderComponent from "../SliderComponent";
-import { postBiometrics } from "../../services/biometrics";
-import { errorMessage } from "../../constants/formErrors";
-//import Slider from "@material-ui/core/Slider";
+import { useForm, Controller } from "react-hook-form";
+import Slider from "react-input-slider";
+
 
 import "./BiometricForm.scss";
 
 export default function BiometricForm() {
-  const { handleSubmit, control } = useForm();
-
-  return (
-    <div className="registerForm_Container">
-      <form onSubmit={handleSubmit(onsubmit)}>
-        <Controller
-          control={control}
-          name="test"
-          render={({ axis, xmax, xmin, xstep, onChange }) => (
-            <SliderComponent
-              axis={"x"}
-              xmax={100}
-              xmin={1}
-              xstep={1}
-              onChange={(x) => onChange(parseInt(x.target.value))}
-            />
-          )}
-        />
-        <button className="registerForm_Container_button" type="submit">
-          Register
-        </button>
-      </form>
-    </div>
-  );
-}
+    const { handleSubmit, control } = useForm();
+  
+    const onSubmit = (data) => console.log(data);
+  
+    return (
+      <div className="registerForm_Container">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Controller
+            control={control}
+            name="test"
+            defaultValue={50}
+            render={({ field: { value, onChange } }) => (
+              <Slider
+                axis={"x"}
+                xmax={100}
+                xmin={1}
+                xstep={1}
+                onChange={({ x }) => onChange(x)}
+                x={value}
+              />
+            )}
+          />
+          <input type="submit" />
+        </form>
+      </div>
+    );
+  }
--- client/src/components/SliderComponent.jsx
+++ client/src/components/SliderComponent.jsx
@@ -1,23 +1,20 @@
 import React from "react";
-import { useState } from "react";
 import Slider from "react-input-slider";
 
 
 export default function SliderComponent(props) {
 
-  const [value, setSlider] = useState({});
-
-  const { axis, xmax, xmin, xstep, onChange } = props;;
+  const { axis, xmax, xmin, xstep, onChange, defaultValue } = props;
 
   return (
     <div>
       <Slider
         axis={axis}
-        x={value.x}
         xmax={xmax}
         xmin={xmin}
         xstep={xstep}
         onChange={onChange}
+        defaultValue={defaultValue}
       />
     </div>
   );
